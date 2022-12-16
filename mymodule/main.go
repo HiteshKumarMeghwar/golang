fd526da9e158d5465faaf1bdd269d9e2aa684bc3
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2022-12-16 15:06:57 +0500

    4rth commit

--- mymodule/main.go
+++ mymodule/main.go
@@ -31,10 +31,27 @@ func todo(w http.ResponseWriter, r *http.Request) {
 	html.Execute(w, data)
 }
 
+func about(w http.ResponseWriter, r *http.Request) {
+	data := PageData{
+		Title: "TODO LIST",
+		Todos: []Todo{
+			{Item: "Install GO", Done: true},
+			{Item: "Learn GO", Done: false},
+			{Item: "Like this vide", Done: false},
+		},
+	}
+
+	html.Execute(w, data)
+}
+
 func main() {
 	mux := http.NewServeMux()
+	html = template.Must(template.ParseFiles("templates/about.html"))
 	html = template.Must(template.ParseFiles("templates/index.html"))
+	mux.HandleFunc("/about", about)
 	mux.HandleFunc("/todo", todo)
-
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
