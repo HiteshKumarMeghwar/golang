890e57f88f2a2be2da66113667eda32785a6816b
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2022-12-17 10:37:25 +0500

    8th commit

--- mymodule/main.go
+++ mymodule/main.go
@@ -6,16 +6,18 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// w.Header().Set("Content-Type", "text/html")
+	// w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-// func about(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprint(w, "<h1>Contact Page<p>To get in touch, email me at <a href=\"www.google.com\">Google</a>.</p>")
-// }
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page<p>To get in touch, email me at <a href=\"/\">Home page</a>.</p>")
+}
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :8080...")
 	http.ListenAndServe(":8080", nil)
 }
