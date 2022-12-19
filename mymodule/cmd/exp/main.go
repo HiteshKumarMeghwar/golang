71064669c5561501e1af8e408943c497354c46d0
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2022-12-19 18:18:16 +0500

    updated

--- mymodule/cmd/exp/main.go
+++ mymodule/cmd/exp/main.go
@@ -1,40 +1,5 @@
 package main
 
-import (
-	"html/template"
-	"os"
-)
-
-type User struct {
-	Name string
-	// Age  int
-	// Meta UserMeta
-	Bio string
-}
-
-/* type UserMeta struct {
-	Visits int
-} */
-
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
-
-	user := User{
-		Name: "Hitesh Kumar",
-		/* Age:  111,
-		Meta: UserMeta{
-			Visits: 4,
-		}, */
-		Bio: `<script>alert("Haha, You have been ....")</script>`,
-	}
-
-	err = t.Execute(os.Stdout, user)
-
-	if err != nil {
-		panic(err)
-	}
 
 }
