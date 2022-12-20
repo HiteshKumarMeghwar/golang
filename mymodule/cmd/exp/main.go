2acbc71d8c49289b0988ed37cabae484f82b30d8
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2022-12-20 18:52:52 +0500

    updated

--- mymodule/cmd/exp/main.go
+++ mymodule/cmd/exp/main.go
@@ -9,20 +9,7 @@ import (
 
 func main() {
 
-	const (
-		host     = "localhost"
-		port     = 5432
-		user     = "baloo"
-		password = "junglebook"
-		dbname   = "lenslocked"
-	)
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	// db, err := sql.Open("pgx", "host=localhost port=5432 user=hiteshkumar password=hitesh dbname=mymodule sslmode=disable")
-	db, err := sql.Open("pgx", psqlInfo)
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=postgres password=postgres dbname=mymodule sslmode=disable")
 
 	if err != nil {
 		panic(err)
