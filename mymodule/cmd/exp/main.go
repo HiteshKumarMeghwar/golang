57482bfc315c4fa9ef4b88615e80571ceb23146b
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2022-12-20 13:06:23 +0500

    updated

--- mymodule/cmd/exp/main.go
+++ mymodule/cmd/exp/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func main() {
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=mymodule sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected")
 }
