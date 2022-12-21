201bde9b6da2ebd8ebeb9b1c172c2a056b125eb6
Hitesh Kumar <hiteshkumarkunri@gmail.com>
2022-12-21 18:12:07 +0500

    updated

--- mymodule/cmd/exp/main.go
+++ mymodule/cmd/exp/main.go
@@ -46,63 +46,6 @@ func main() {
 
 	fmt.Println("Connected")
 
-	/* Create Tables */
-	/* _, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS users (
-			id SERIAL PRIMARY KEY,
-			name TEXT,
-			email TEXT UNIQUE NOT NULL
-		);
-
-		CREATE TABLE IF NOT EXISTS orders (
-			id SERIAL PRIMARY KEY,
-			user_id INT NOT NULL,
-			amount INT,
-			description TEXT
-		);
-	`)
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Tables created")
-	*/
-	/* Insert Data into Tables */
-	/* _, err = db.Exec(`
-		INSERT INTO users (name, email) VALUES ('Hitesh Kumar','hiteshkumar@gmail.com');
-	`) */
-
-	/* name := "Arveer"
-	email := "arveer@gmail.com"
-
-	row := db.QueryRow(`
-		INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id;
-	`, name, email)
-	var id int
-	err = row.Scan(&id)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("User created. id = ", id) */
-
-	/* Multiple order records creating */
-	/* userID := 1
-	for i := 1; i <= 10; i++ {
-		amount := i * 100
-		desc := fmt.Sprintf("Fake order #%d", i)
-
-		_, err := db.Exec(`
-			INSERT INTO orders (user_id, amount, description) VALUES ($1, $2, $3)
-		`, userID, amount, desc)
-
-		if err != nil {
-			panic(err)
-		}
-	}
-	fmt.Println("multiple orders created...") */
-
 	/* Querying Multiple records */
 	type Order struct {
 		ID          int
